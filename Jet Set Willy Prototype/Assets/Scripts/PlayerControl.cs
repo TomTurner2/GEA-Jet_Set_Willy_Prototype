a136b6dbf54d8ff4a33ebc3fbe04a5850595299c
Tom Turner <tom3.turner@live.uwe.ac.uk>
2017-01-17 19:09:37 +0000

    Basic Player controller
    +simple movement system
    +checks if grounded using circle cast
    +can prevent from jumping on certain surfaces/objects by adding them to
    the no jump layer
    +player has no air control to mimic original

---
+++ Jet Set Willy Prototype/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    const float REDUCE_CAST_RADIUS = 0.05f;
+    private LayerMask jumpable = 0;
+    private Rigidbody2D myRB = null;
+    private CircleCollider2D myCollider = null;
+    private bool grounded = true;
+
+    [Tooltip("Players movement speed")]
+    public float speed = 6.0f;
+    [Tooltip("Players jump force (jump height)")]
+    public float jumpForce = 6.0f;
+    [Tooltip("Distance between floor and player you can jump")]
+    public float jumpTolerance = 0.1f;
+
+
+    void Start ()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<CircleCollider2D>();
+        jumpable = 1 << LayerMask.NameToLayer("No Jump");//get objects on no jump layer 
+        jumpable = ~(jumpable);//invert so we ground check only desired objects 
+
+    }
+
+
+
+    void FixedUpdate ()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        if ((/*Input.GetButtonDown("Jump")||*/Input.GetButton("Jump")) && checkGrounded())
+        {
+            myRB.velocity = new Vector2(myRB.velocity.x + movement, jumpForce);//jump player using velocity
+            grounded = false;
+        }
+        
+        
+        if (checkGrounded())
+        {
+            myRB.velocity = new Vector2(movement * speed, myRB.velocity.y);//move player using velocity    
+        }
+    }
+
+
+
+    /// <summary>
+    /// Checks if the player is touching the ground using a Circle cast.
+    /// Uses jump tolerance and player height to determine cast length.
+    /// Will ignore anything on the 'No Jump' layers.
+    /// </summary>
+    /// <returns>If the player is grounded</returns>
+    private bool checkGrounded()
+    {
+        if(Physics2D.CircleCast(transform.position, myCollider.radius - REDUCE_CAST_RADIUS,-transform.up, myCollider.radius/2 + jumpTolerance, jumpable))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
