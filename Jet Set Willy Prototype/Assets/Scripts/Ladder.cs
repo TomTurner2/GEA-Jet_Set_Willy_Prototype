18406faed1603c6d72987414b4da32cc0ef3c160
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-01-24 11:35:28 +0000

    Press down to drop done

---
+++ Jet Set Willy Prototype/Assets/Scripts/Ladder.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ladder : MonoBehaviour
+{
+    private PlayerControl player;
+    private Sprite ladderSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+           // player.on_ladder = true;
+           //ladderSprite.texture.
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            //player.on_ladder = false;
+        }
+    }
+}
+
+//TO GO IN PLAYER SCRIPT
+/*
+//Ladder movement
+
+    Private float gravityStore = 0;
+    public flaot climbSpeed = 5.0f;
+
+        if(on_ladder)
+        {
+            rigid_body.gravityScale = 0;
+            climb_velocity = climb_speed * Input.GetAxisRaw("Vertical");
+            rigid_body.velocity = new Vector2(rigid_body.velocity.x, climb_velocity);
+        }
+        if (!on_ladder)
+        {
+            rigid_body.gravityScale = gravity_store;
+        }
+    */
--- Jet Set Willy Prototype/Assets/Scripts/oneWayPlatform.cs
+++ Jet Set Willy Prototype/Assets/Scripts/oneWayPlatform.cs
@@ -3,23 +3,50 @@ using System.Collections;
 
 public class oneWayPlatform : MonoBehaviour {
     public BoxCollider2D platform;
+
     private bool oneWay = false;
+    private bool dropDown = false;
+
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
+
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         //enable solid collider whenever bottom trigger isn't entered
         platform.enabled = !oneWay;
+
+        //move input over to player script
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            dropDown = true;
+        }
+        else
+        {
+            dropDown = false;
+        }
+        //if down key is pressed, player falls through
+        if(dropDown == true)
+        {
+            oneWay = true;
+        }
+
 	}
+
+
 
     void OnTriggerStay2D(Collider2D col)
     {
         oneWay = true;
     }
+
+
 
     void OnTriggerExit2D(Collider2D col)
     {
