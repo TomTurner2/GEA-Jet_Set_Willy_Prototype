08f1485d6e14a3728e0962c6c17c94033c198dcb
Curtis Wiseman <curtis2.wiseman@live.uwe.ac.uk>
2017-01-26 17:45:08 +0000

    Worked some more on scene switching, figuring out the pseudocode.

--- Jet Set Willy Prototype/Assets/Scripts/ScreenEdgeMethod2.cs
+++ Jet Set Willy Prototype/Assets/Scripts/ScreenEdgeMethod2.cs
@@ -7,22 +7,26 @@ public class ScreenEdgeMethod2 : MonoBehaviour
     public GameObject mainCam;
     public GameObject player;
     public char direction;
-    public int sizeX;
-    public int sizeY;
+    private const int sizeX = 16;
+    private const int sizeY = 9;
 
     public float timeTakenDuringLerp = 1f;
 
     private bool _isLerping = false;
 
-    private Vector3 _camStartPosition;
-    private Vector3 _camEndPosition;
+    private Vector3 _sceneStartPosition;
+    private Vector3 _sceneEndPosition;
+	private Vector3 _nextSceneStartPosition;
+	private Vector3 _nextSceneEndPosition;
     private Vector3 _pStartPosition;
     private Vector3 _pEndPosition;
 
     private float _timeStartedLerping;
 
 	public string target_scene;
+	public GameObject target_scene_objects;
 	public string current_scene;
+	public GameObject current_scene_objects;
 
 
     void StartLerping()
@@ -30,30 +34,36 @@ public class ScreenEdgeMethod2 : MonoBehaviour
         _isLerping = true;
         _timeStartedLerping = Time.time;
 
-        _camStartPosition = mainCam.transform.position;
-        _camEndPosition = _camStartPosition;
+		_sceneStartPosition = current_scene_objects.transform.position;
+        _sceneEndPosition = _sceneStartPosition;
 
         _pStartPosition = player.transform.position;
         _pEndPosition = _pStartPosition;
 
+		_nextSceneEndPosition = new Vector3(0, 0, 0);
+
         switch (direction)
         {
-            case 'u':
-                _camEndPosition += new Vector3(0, sizeY, 0);
-                _pEndPosition += new Vector3(0, 1.5f, 0);
-                break;
-            case 'd':
-                _camEndPosition += new Vector3(0, -sizeY, 0);
-                _pEndPosition += new Vector3(0, -1.5f, 0);
-                break;
-            case 'l':
-                _camEndPosition += new Vector3(-sizeX, 0, 0);
-                _pEndPosition += new Vector3(-1.5f, 0, 0);
-                break;
-            case 'r':
-                _camEndPosition += new Vector3(sizeX, 0, 0);
-                _pEndPosition += new Vector3(1.5f, 0, 0);
-                break;
+		case 'u':
+			_sceneEndPosition += new Vector3 (0, -sizeY, 0);
+			_pEndPosition += new Vector3 (0, -sizeY + 1.5f, 0);
+			_nextSceneStartPosition = new Vector3(0, sizeY, 0);
+            break;
+        case 'd':
+            _sceneEndPosition += new Vector3(0, sizeY, 0);
+			_pEndPosition += new Vector3(0, sizeY - 1.5f, 0);
+			_nextSceneStartPosition = new Vector3(0, -sizeY, 0);
+			break;
+		case 'l':
+			_sceneEndPosition += new Vector3 (sizeX, 0, 0);
+			_pEndPosition += new Vector3 (sizeX - 1.5f, 0, 0);
+			_nextSceneStartPosition = new Vector3(-sizeX, 0, 0);
+			break;
+		case 'r':
+			_sceneEndPosition += new Vector3 (-sizeX, 0, 0);
+			_pEndPosition += new Vector3 (-sizeX + 1.5f, 0, 0);
+			_nextSceneStartPosition = new Vector3(sizeX, 0, 0);
+			break;
         }
 		player.SendMessage ("freeze", true);
     }
@@ -72,30 +82,29 @@ public class ScreenEdgeMethod2 : MonoBehaviour
             float timeSinceStarted = Time.time - _timeStartedLerping;
             float percentageComplete = timeSinceStarted / timeTakenDuringLerp;
 
-            mainCam.transform.position = Vector3.Lerp(_camStartPosition, _camEndPosition, percentageComplete);
+			current_scene_objects.transform.position = Vector3.Lerp(_sceneStartPosition, _sceneEndPosition, percentageComplete);
+			target_scene_objects.transform.position = Vector3.Lerp(_nextSceneStartPosition, _nextSceneEndPosition, percentageComplete);
             player.transform.position = Vector3.Lerp(_pStartPosition, _pEndPosition, percentageComplete);
 
             if (percentageComplete >= 1.0f)
             {
                 _isLerping = false;
 				player.SendMessage ("freeze", false);
-				SceneManager.UnloadScene (current_scene);
+				SceneManager.UnloadScene(current_scene);
             }
         }
     }
 
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(BoxCollider2D col)
     {
         if (col.gameObject == player)
 		{
             //Scrolls to the next scene, loads in scene from file?
+			SceneManager.LoadScene (target_scene, LoadSceneMode.Additive);
+			target_scene_objects = SceneManager.GetSceneByName(target_scene).GetRootGameObjects()[0];
 
             StartLerping();
-
-			//current_scene = SceneManager.GetActiveScene ().name;
-			//Application.LoadLevelAdditive (target_scene);
-			SceneManager.LoadScene ("Tom", LoadSceneMode.Additive);
         }
     }
 }
