5d6d41c209f20062cd7f67f981afa80c204b9ad6
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2017-01-29 13:39:48 +0000

    Minor bug fixes and ladder colour change
    - Tried replicating the ladder colour change that the original game
    uses, looks a bit janky but it's okay for now.

--- Jet Set Willy Prototype/Assets/Scripts/Ladder.cs
+++ Jet Set Willy Prototype/Assets/Scripts/Ladder.cs
@@ -6,15 +6,13 @@ public class Ladder : MonoBehaviour
     private PlayerControl player = null;
     private SpriteRenderer ladderSprite = null;
     public GameObject graphicObject = null;
-
     public SpriteSet sprites;
 
     // Use this for initialization
     void Start()
     {
         player = FindObjectOfType<PlayerControl>();
-       // ladderSprite = graphicObject.GetComponent<SpriteRenderer>();
-      //  ladderSprite.sprite = sprites.idle;
+        ladderSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,16 +26,17 @@ public class Ladder : MonoBehaviour
         if (col.tag == "Player")
         {
             player.onLadder = true;
-            //ladderSprite.texture.
-           // ladderSprite.sprite = sprites.idle;
+            ladderSprite.color = new Color(0.0f, 1.0f, 0.0f, 1f); // Set to Green
         }
     }
+
 
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
             player.onLadder = false;
+            ladderSprite.color = new Color(1f, 1f, 1f, 1f); // Set to opaque gray
         }
     }
 }
