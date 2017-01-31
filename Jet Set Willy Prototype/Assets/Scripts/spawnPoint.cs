4701838a815270d71052d339b13a97f58dc978e5
Tom Turner <tom3.turner@live.uwe.ac.uk>
2017-01-31 13:36:46 +0000

    Spawn points for static level
    +simple spawn point switching for the level without additive loading

---
+++ Jet Set Willy Prototype/Assets/Scripts/spawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("changing");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+          
+            collision.GetComponent<PlayerControl>().respawnPoint = this.transform;
+        }
+    }
+
+}
